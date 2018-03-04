e74034eb839b21593b3f12fa59f8abb543420f62
junggeunhee <gh.jung1@albamapp.com>
2018-03-04 23:02:52 +0900

    기능: add, remove, edit, change 완료

--- js/todo.js
+++ js/todo.js
@@ -1,37 +1,72 @@
 const todoInit = (() => {
   let id = 0;
   let todoList = {};
+  let showCreatePop = false;
 
   const init = () => {
-    console.log('------ 현재 상태 ------');
-    console.log(todoList);
+    console.group('Todo Servie List');
+    console.log('--- add ( "내용" )');
+    console.log('--- remove ( id )');
+    console.log('--- edit ( id, "내용" )');
+    console.log('--- change ( id )');
+    console.groupEnd();
+    const todoListUl = document.querySelector('.todo-list');
+    todoListUl.innerHTML = '';
+  }
+
+  const render = () => {
+    let i = 0;
+    let MAX = Object.keys(todoList).length;
+    const targetEle = document.querySelector('.todo-list');
+    init();
+    for (i; i < MAX; i++) {
+      if (todoList[i]) {
+        targetEle.innerHTML += `<li>${todoList[i].id}${todoList[i].title}</li>`;
+      }    
+    }
   }
 
   const addList = (title) => {
-    id++;
+    if (!title) return Error('Todo에 Title이 비어있습니다. 추가해주세요!');
     todoList = {
       ...todoList,
       [id]: {
-        ...todoList[id],
-        'id': id,
-        'title': title,
-        'status': 'progress'
+        id,
+        title,
+        status: 'progress'
       }
     }
-    init();
-    return todoList;
+    id++;
+    render();      
   }
 
   const toggleTodo = (id) => {
     todoList[id].status = 'success';
-    init();
+    render();
     return todoList;
   }
 
+  const deleteTodo = (id) => {
+    delete todoList[id];
+    render();
+    return todoList;
+  };
+
+  const editTodo = (id, title) => {
+    const newTitle = title;
+    todoList[id].title = newTitle;
+    render();
+    return todoList;
+  };
+
   return {
-    addTodo: addList,
-    toggle: toggleTodo,
+    init: init,
+    add: addList,
+    change: toggleTodo,
+    remove: deleteTodo,
+    edit: editTodo,
+    render: render,
   }
 })();
+todoInit.init();
 
-todoInit.addTodo('밥먹자');
