e84059cb2a87933c1253a5f16107ec48a239904b
junggeunhee <gh.jung1@albamapp.com>
2018-03-05 01:33:17 +0900

    버튼 기능 추가 및 코드 정리

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,31 @@ const todoInit = (() => {
   let todoList = {};
   let showCreatePop = false;
 
+  class EventBindEle {
+    constructor(target, viewEle) {
+      this.eventTarget = document.querySelector(target);
+      this.viewEle = document.querySelector(viewEle);
+    }
+  }
+
+  const toggleCreatePop = () => {
+    const initEle = new EventBindEle('.create-todo', '.create-todo-pop');
+    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
+    initEle.eventTarget.addEventListener('click', () => {
+      showCreatePop = true;
+      initEle.viewEle.classList.add('on-pop');
+    });
+  }
+
+  const toggleCloseBtn = () => {
+    const initEle = new EventBindEle('.close-create-pop', '.create-todo-pop');
+    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
+    initEle.eventTarget.addEventListener('click', () => {
+      showCreatePop = false;
+      initEle.viewEle.classList.remove('on-pop');
+    });
+  }
+
   const init = () => {
     console.group('Todo Servie List');
     console.log('--- add ( "내용" )');
@@ -10,6 +35,8 @@ const todoInit = (() => {
     console.log('--- edit ( id, "내용" )');
     console.log('--- change ( id )');
     console.groupEnd();
+    toggleCreatePop();
+    toggleCloseBtn();
     const todoListUl = document.querySelector('.todo-list');
     todoListUl.innerHTML = '';
   }
@@ -20,9 +47,11 @@ const todoInit = (() => {
     const targetEle = document.querySelector('.todo-list');
     init();
     for (i; i < MAX; i++) {
-      if (todoList[i]) {
-        targetEle.innerHTML += `<li>${todoList[i].id}${todoList[i].title}</li>`;
-      }    
+      if (todoList.hasOwnProperty(id)) {
+        targetEle.innerHTML += todoList[i] === 'success'
+        ?`<li>${todoList[i].id}${todoList[i].title}<span>O</span></li>`
+        :`<li>${todoList[i].id}${todoList[i].title}</li>`;
+      }
     }
   }
 
@@ -41,18 +70,21 @@ const todoInit = (() => {
   }
 
   const toggleTodo = (id) => {
+    if (!todoList.hasOwnProperty(id)) return Error('상태를 변경할 Todo가 없습니다.');
     todoList[id].status = 'success';
     render();
     return todoList;
   }
 
   const deleteTodo = (id) => {
+    if (!todoList.hasOwnProperty(id)) return Error('삭제할 Todo가 없습니다.');
     delete todoList[id];
     render();
     return todoList;
   };
 
   const editTodo = (id, title) => {
+    if (!todoList.hasOwnProperty(id)) return Error('변경할 Todo가 없습니다.');
     const newTitle = title;
     todoList[id].title = newTitle;
     render();
@@ -69,4 +101,3 @@ const todoInit = (() => {
   }
 })();
 todoInit.init();
-
