74544c1414de659810548f7186a5bcb3991fa180
junggeunhee <gh.jung1@albamapp.com>
2018-03-05 10:59:05 +0900

    status값 상수로 빼고, id값 클로저로 스코프만듬.

--- js/todo.js
+++ js/todo.js
@@ -1,42 +1,19 @@
 const todoInit = (() => {
   let id = 0;
   let todoList = {};
-  let showCreatePop = false;
-
-  class EventBindEle {
-    constructor(target, viewEle) {
-      this.eventTarget = document.querySelector(target);
-      this.viewEle = document.querySelector(viewEle);
-    }
-  }
-
-  const toggleCreatePop = () => {
-    const initEle = new EventBindEle('.create-todo', '.create-todo-pop');
-    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
-    initEle.eventTarget.addEventListener('click', () => {
-      showCreatePop = true;
-      initEle.viewEle.classList.add('on-pop');
-    });
-  }
-
-  const toggleCloseBtn = () => {
-    const initEle = new EventBindEle('.close-create-pop', '.create-todo-pop');
-    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
-    initEle.eventTarget.addEventListener('click', () => {
-      showCreatePop = false;
-      initEle.viewEle.classList.remove('on-pop');
-    });
-  }
+  const STATE_P = 'progress';
+  const STATE_C = 'success';
 
   const init = () => {
-    console.group('Todo Servie List');
+    console.group('TODO 명령어 모음');
     console.log('--- add ( "내용" )');
     console.log('--- remove ( id )');
     console.log('--- edit ( id, "내용" )');
     console.log('--- change ( id )');
     console.groupEnd();
-    toggleCreatePop();
-    toggleCloseBtn();
+    console.group('현재 TODO 상태');
+    console.log('todoList --- ', todoList);
+    console.groupEnd();
     const todoListUl = document.querySelector('.todo-list');
     todoListUl.innerHTML = '';
   }
@@ -49,29 +26,35 @@ const todoInit = (() => {
     for (i; i < MAX; i++) {
       if (todoList.hasOwnProperty(id)) {
         targetEle.innerHTML += todoList[i] === 'success'
-        ?`<li>${todoList[i].id}${todoList[i].title}<span>O</span></li>`
-        :`<li>${todoList[i].id}${todoList[i].title}</li>`;
+        ?`<li>${todoList[i].id}  ---  ${todoList[i].title}<span>O</span></li>`
+        :`<li>${todoList[i].id}  ---  ${todoList[i].title}</li>`;
       }
     }
   }
+ 
+  const addList = (() => {
+    let id = 0;
 
-  const addList = (title) => {
-    if (!title) return Error('Todo에 Title이 비어있습니다. 추가해주세요!');
-    todoList = {
-      ...todoList,
-      [id]: {
-        id,
-        title,
-        status: 'progress'
+    return (title) => {
+      if (!title) return Error('Todo에 Title이 비어있습니다. 추가해주세요!');
+      todoList = {
+        ...todoList,
+        [id]: {
+          id,
+          title,
+          status: STATE_P
+        },
       }
+      id++;
+      render();      
     }
-    id++;
-    render();      
-  }
-
+  })();
+  
   const toggleTodo = (id) => {
     if (!todoList.hasOwnProperty(id)) return Error('상태를 변경할 Todo가 없습니다.');
-    todoList[id].status = 'success';
+    todoList[id].status = todoList[id].status === STATE_P 
+      ? STATE_C
+      : STATE_P;
     render();
     return todoList;
   }
@@ -100,4 +83,41 @@ const todoInit = (() => {
     render: render,
   }
 })();
+
+const todoEventInit = (() => {
+  let showCreatePop = false;
+
+  class EventBindEle {
+    constructor(target, viewEle) {
+      this.eventTarget = document.querySelector(target);
+      this.viewEle = document.querySelector(viewEle);
+    }
+  }
+
+  const toggleCreatePop = () => {
+    const initEle = new EventBindEle('.create-todo', '.create-todo-pop');
+    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
+    initEle.eventTarget.addEventListener('click', () => {
+      showCreatePop = true;
+      initEle.viewEle.classList.add('on-pop');
+    });
+  }
+
+  const toggleCloseBtn = () => {
+    const initEle = new EventBindEle('.close-create-pop', '.create-todo-pop');
+    if (!(initEle instanceof EventBindEle)) return Error('createPopUp Target is Not EventBindEle instacne');
+    initEle.eventTarget.addEventListener('click', () => {
+      showCreatePop = false;
+      initEle.viewEle.classList.remove('on-pop');
+    });
+  }
+
+  return {
+    init: function() {
+      toggleCreatePop();
+      toggleCloseBtn();
+    }
+  }
+})();
+todoEventInit.init();
 todoInit.init();
